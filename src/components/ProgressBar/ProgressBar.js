7febce670fcbb10c444e4ed461943c1dcbdd50f3
tripplybrand <tripplybrand@gmail.com>
2021-07-14 15:13:51 -0400

    ProgressBar the way Josh suggests.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -8,15 +8,18 @@ import VisuallyHidden from '../VisuallyHidden';
 const SIZES = {
   small: {
     "--height": 8 + 'px',
-    "--padding": 0 + 'px'
+    "--padding": 0 + 'px',
+    "--border-radius": 4 + 'px'
   },
   medium: {
     "--height": 12 + 'px',
-    "--padding": 0 + 'px'
+    "--padding": 0 + 'px',
+    "--border-radius": 4 + 'px'
   },
   large: {
-    "--height": 24 + 'px',
-    "--padding": 4 + 'px'
+    "--height": 16 + 'px',
+    "--padding": 4 + 'px',
+    "--border-radius": 8 + 'px'
   }
 }
 
@@ -24,51 +27,39 @@ const ProgressBar = ({ value, size }) => {
 
   const styles = SIZES[size];
 
-  let ProgressBarMarker
-  size === 'small' ? ProgressBarMarker = ProgressBarMarkerSmall : ProgressBarMarker = ProgressBarMarkerLarge
-  if (size === "small") {
-    ProgressBarMarker = ProgressBarMarkerSmall;
-  } else if (size === "medium") {
-    ProgressBarMarker = ProgressBarMarkerMedium;
-  } else if (size === "large") {
-    ProgressBarMarker = ProgressBarMarkerLarge;
-  } else {
-    throw new Error(`Unrecognized Button size: ${size}`);
+  if(!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`)
   }
 
   const valueMax = 100
   return <ProgressBarContainer style={styles} role="progressbar" 
     aria-valuenow={value} aria-valuemin="0" aria-valuemax={valueMax}>
-     <ProgressBarMarker value={value/valueMax}></ProgressBarMarker>
+      <ProgressBarMarkerWrapper>
+        <ProgressBarMarker style={{'--width': value + '%'}}>
+          <VisuallyHidden>{value}</VisuallyHidden>
+        </ProgressBarMarker>
+     </ProgressBarMarkerWrapper>
   </ProgressBarContainer>
 };
 
 export default ProgressBar;
 
 const ProgressBarContainer = styled.div`
-width: 370px;
-height: var(--height);
-padding: var(--padding);
-background: ${COLORS.transparentGray15};
+background-color: ${COLORS.transparentGray15};
 box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-border-radius: 8px;
+border-radius: var(--border-radius);
+padding: var(--padding);
 `
 
-const ProgressBarMarkerBase = styled.div`
-height: 100%;
+const ProgressBarMarkerWrapper = styled.div`
+border-radius: 4px;
+overflow: hidden;
+`
+
+const ProgressBarMarker = styled.div`
+width: var(--width);
+height: var(--height);
 background: ${COLORS.primary};
-border-radius: ${props => props.value >= .98 ? '4px 4px 4px 4px' : '4px 0px 0px 4px' };
+border-radius: '4px 0px 0px 4px';
 `
 
-//Subtract padding left and right from 370. Should I have done this as an imported constant?
-const ProgressBarMarkerSmall = styled(ProgressBarMarkerBase)`
-  width: ${(props => props.value * 370)}px;
-`
-
-const ProgressBarMarkerMedium = styled(ProgressBarMarkerBase)`
-  width: ${(props => props.value * 370)}px;
-`
-
-const ProgressBarMarkerLarge = styled(ProgressBarMarkerBase)`
-  width: ${(props => props.value * 362)}px;
-`
---
+++ src/components/ProgressBar/ProgressBar_Attempt.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import styled from 'styled-components';
+
+import { COLORS } from '../../constants';
+import VisuallyHidden from '../VisuallyHidden';
+
+const SIZES = {
+  small: {
+    "--height": 8 + 'px',
+    "--padding": 0 + 'px'
+  },
+  medium: {
+    "--height": 12 + 'px',
+    "--padding": 0 + 'px'
+  },
+  large: {
+    "--height": 24 + 'px',
+    "--padding": 4 + 'px'
+  }
+}
+
+const ProgressBar = ({ value, size }) => {
+
+  const styles = SIZES[size];
+
+  let ProgressBarMarker
+  size === 'small' ? ProgressBarMarker = ProgressBarMarkerSmall : ProgressBarMarker = ProgressBarMarkerLarge
+  if (size === "small") {
+    ProgressBarMarker = ProgressBarMarkerSmall;
+  } else if (size === "medium") {
+    ProgressBarMarker = ProgressBarMarkerMedium;
+  } else if (size === "large") {
+    ProgressBarMarker = ProgressBarMarkerLarge;
+  } else {
+    throw new Error(`Unrecognized Button size: ${size}`);
+  }
+
+  const valueMax = 100
+  return <ProgressBarContainer style={styles} role="progressbar" 
+    aria-valuenow={value} aria-valuemin="0" aria-valuemax={valueMax}>
+     <ProgressBarMarker value={value/valueMax}>
+       <VisuallyHidden>{value/valueMax}</VisuallyHidden>
+     </ProgressBarMarker>
+  </ProgressBarContainer>
+};
+
+export default ProgressBar;
+
+const ProgressBarContainer = styled.div`
+width: 370px;
+height: var(--height);
+padding: var(--padding);
+background: ${COLORS.transparentGray15};
+box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+border-radius: 8px;
+`
+
+const ProgressBarMarkerBase = styled.div`
+height: 100%;
+background: ${COLORS.primary};
+border-radius: ${props => props.value >= .98 ? '4px 4px 4px 4px' : '4px 0px 0px 4px' };
+`
+
+//Subtract padding left and right from 370. Should I have done this as an imported constant?
+const ProgressBarMarkerSmall = styled(ProgressBarMarkerBase)`
+  width: ${(props => props.value * 370)}px;
+`
+
+const ProgressBarMarkerMedium = styled(ProgressBarMarkerBase)`
+  width: ${(props => props.value * 370)}px;
+`
+
+const ProgressBarMarkerLarge = styled(ProgressBarMarkerBase)`
+  width: ${(props => props.value * 362)}px;
+`
